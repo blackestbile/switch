719cd55240963639161edee3fcf2a2e8b457ae0f
blackestbile <soldego@riseup.net>
2024-07-16 08:43:47 -0300

    Create sw3.php

---
+++ sw3.php
@@ -0,0 +1,15 @@
+<?php
+$i = 2;
+
+switch ($i) {
+    case 0:
+            print ("i é igual a zero");
+        break;
+    case 1:
+            print ("i é igual a um");
+        break;
+    case 2:
+            print ("i é igual a dois");
+        break;
+}
+?>
